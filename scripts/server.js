494f5c20e04039b230834e219267dd9ae2f2cc04
shade <shadedr34m@gmail.com>
2024-04-23 20:22:04 +0300

    Handled Post Request From Create Route

--- scripts/server.js
+++ scripts/server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 
+// Body Parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // Public Files
 app.use(express.static("public"));
 
@@ -11,6 +15,13 @@ app.set("view engine", "ejs");
 // Create Route
 app.get("/create", (req, res) => {
     res.render("create.ejs");
+});
+
+// Handling Post Request From /create 
+app.post("/create", (req, res) => {
+    const profileName = req.body.profileName;
+
+    res.redirect(`/view/${profileName}`);
 });
 
 // Starting The Server

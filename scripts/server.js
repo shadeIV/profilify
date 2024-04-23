fc379b7350efe7a9edafb5933f5d8eb7c1b95c39
shade <shadedr34m@gmail.com>
2024-04-23 13:11:52 +0300

    Set The View Engine And /create Route

--- scripts/server.js
+++ scripts/server.js
@@ -2,6 +2,14 @@
 const express = require("express");
 const app = express();
 
+// View Engine
+app.set("view engine", "ejs");
+
+// Create Route
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
 // Starting The Server
 const port = 8080;
 app.listen(port, () => {

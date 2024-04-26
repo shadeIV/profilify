f6eccc8afd88e565df4a3e86bf3983491b48420f
shade <shadedr34m@gmail.com>
2024-04-26 19:17:09 +0300

    Added Delete Algorithm

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,26 +1,49 @@
 // Importing Storage Functions
 import { storage, getStorage, saveToStorage } from "./storage.js";
 
-// Rendering Html
-let htmlCode = "";
+// Rendering Profiles
+function renderProfiles() {
+    let htmlCode = "";
 
-getStorage();
-storage.forEach((profile) => {
-    htmlCode += `
-    <a href="/view/${profile.profileId}">
-        <div class="profile-div scroll-animation">
+    getStorage();
+    storage.forEach((profile) => {
+        htmlCode += `
+    <div class="profile-div scroll-animation">
+        <a class="view-link" href="/view/${profile.profileId}">
             <img class="profile-image" src="${profile.profilePicture}">
             <p class="profile-name">${profile.profileName}</p>
-        </div>
-    </a> 
+        </a> 
+        <button data-profile-id="${profile.profileId}" class="delete-profile-button">Delete Profile</button>
+    </div>
     `;
-});
+    });
 
-if (htmlCode) {
-    document.querySelector(".profiles").innerHTML = htmlCode;
-} else if (htmlCode == "") { document.querySelector(".profiles").innerHTML = '<p class="no-profile-p">No Profiles Created...</p>' }
+    // Checking If There Is No Profile
+    if (htmlCode) {
+        document.querySelector(".profiles").innerHTML = htmlCode;
+    } else if (htmlCode == "") { document.querySelector(".profiles").innerHTML = '<p class="no-profile-p">No Profiles Created...</p>' }
+}
 
-// Scroll Animation
+// Rendering Buttons
+function renderButtons() {
+    const deleteButtons = document.querySelectorAll(".delete-profile-button");
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const profileId = button.dataset.profileId;
+
+            getStorage();
+            storage.forEach((profile, profileIndex) => {
+                if (profile.profileId === profileId) {
+                    storage.splice(profileIndex, 1);
+                    localStorage.setItem("storage", JSON.stringify(storage));
+                    window.location.reload();
+                }
+            });
+        });
+    });
+}
+
+// Adding Scroll Animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -28,5 +51,11 @@ const observer = new IntersectionObserver((entries) => {
         } else { entry.target.classList.remove("show"); };
     })
 });
+
+// Startup Code
+
+renderProfiles();
+renderButtons();
+
 const hiddenElements = document.querySelectorAll(".scroll-animation");
 hiddenElements.forEach((el) => observer.observe(el));

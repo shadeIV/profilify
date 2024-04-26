6836d51849f82961a43730ba27f2e541eb338860
shade <shadedr34m@gmail.com>
2024-04-26 16:51:09 +0300

    Added Animation On Scroll

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -8,7 +8,7 @@ getStorage();
 storage.forEach((profile) => {
     htmlCode += `
     <a href="/view/${profile.profileId}">
-        <div class="profile-div">
+        <div class="profile-div scroll-animation">
             <img class="profile-image" src="${profile.profilePicture}">
             <p class="profile-name">${profile.profileName}</p>
         </div>
@@ -19,3 +19,14 @@ storage.forEach((profile) => {
 if (htmlCode) {
     document.querySelector(".profiles").innerHTML = htmlCode;
 } else if (htmlCode == "") { document.querySelector(".profiles").innerHTML = '<p class="no-profile-p">No Profiles Created...</p>' }
+
+// Scroll Animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else { entry.target.classList.remove("show"); };
+    })
+});
+const hiddenElements = document.querySelectorAll(".scroll-animation");
+hiddenElements.forEach((el) => observer.observe(el));
